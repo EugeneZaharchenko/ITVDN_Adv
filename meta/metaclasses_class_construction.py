d4cd7e6acc74d49a8a2e0cf645750b1c78a1a5a1
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-12-12 13:38:06 +0200

    meta additional; numpy init

--- meta/metaclasses_class_construction.py
+++ meta/metaclasses_class_construction.py
@@ -5,7 +5,6 @@ class A:
     def f(self):
         return 42
 
-
 def make_A():
     name = 'A'
     bases = ()
@@ -19,30 +18,26 @@ def make_A():
     namespace = {'a': a, 'b': b, 'f': f}
     A = type(name, bases, namespace)
     return A
-
-
 A = make_A()
-
 
 def make_A_more_accurate():
     name = 'A'
     bases = ()
     namespace = type.__prepare__(name, bases)
     body = (
-        """
+"""
 a = 1
 b = 'hello'
 
 def f(self):
     return 42
 """
-    )
+)
     exec(body, globals(), namespace)
     A = type(name, bases, namespace)
     return A
-
-
 A = make_A_more_accurate()
+
 
 
 def main():
